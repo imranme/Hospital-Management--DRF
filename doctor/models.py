0ae1b09439449d213adfcc4133fe574edb7dd2cd
imranme <tusharimran914076@gmail.com>
2025-03-10 23:54:45 +0600

    Creating doctor Model

--- doctor/models.py
+++ doctor/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from patient.models import Patient
 
 # Create your models here.
+
+class Specialization(models.Model):
+    name = models.CharField(max_length = 30)
+    slug = models.SlugField(max_length = 40)
+    def __str__(self):
+        return self.name
+class Designation(models.Model):
+    name = models.CharField(max_length = 30)
+    slug = models.SlugField(max_length = 40)
+    def __str__(self):
+            return self.name
+class AvailableTime(models.Model):
+    name = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return self.name
+
+# one to many --> many part e kintu foreign key add kortam
+class Doctor(models.Model):
+    user = models.OneToOneField(User, on_delete = models.CASCADE)
+    image = models.ImageField(upload_to="doctor/images/")
+    designation = models.ManyToManyField(Designation)
+    specialization =  models.ManyToManyField(Specialization)
+    available_time = models.ManyToManyField(AvailableTime)
+    fee = models.IntegerField()
+    meet_link = models.CharField(max_length = 100)
+    
+    def __str__(self):
+        return f"{self.user.first_name} {self.user.last_name}"
