a42c331906c6809f41224173be10787573a8bb4e
imranme <tusharimran914076@gmail.com>
2025-03-11 23:04:29 +0600

    Creating serializers Patient Api

--- smart_care/urls.py
+++ smart_care/urls.py
@@ -23,6 +23,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('contact_us/', include('contact_us.urls')),
     path('services/', include('service.urls')),
+    path('patient/', include('patient.urls')),
+    # path('doctor/', include('doctor.urls')),
+    # path('appointment/', include('appointment.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
