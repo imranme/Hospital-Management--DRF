0c24e16ddfc590d544b74b66b8ece69c667a1a8d
imranme <tusharimran914076@gmail.com>
2025-03-13 23:23:11 +0600

    authentication and registration email conformatio error

--- patient/views.py
+++ patient/views.py
@@ -7,11 +7,12 @@ from rest_framework.response import Response
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
+from django.contrib.auth.models import User
 
 # for sending email
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
-# from django.shortcuts import redirect 
+from django.shortcuts import redirect 
 
 class PatientViewset(viewsets.ModelViewSet):
     queryset = models.Patient.objects.all()
@@ -39,3 +40,17 @@ class UserRegistrationApiView(APIView):
             email.send()
             return Response("Check your mail for confirmation")
         return Response(serializer.errors)
+
+def activate(request, uid64, token):
+    try:
+        uid = urlsafe_base64_decode(uid64).decode()
+        user = User._default_manager.get(pk=uid)
+    except(User.DoesNotExist):
+        user = None 
+    
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        return redirect('login')
+    else:
+        return redirect('register')
