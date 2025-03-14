57152cc599e2ec74fc2227da05b89a2b0650c448
imranme <tusharimran914076@gmail.com>
2025-03-14 23:46:02 +0600

    authentication and registration email conformatio error solve

--- patient/views.py
+++ patient/views.py
@@ -51,6 +51,6 @@ def activate(request, uid64, token):
     if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
-        return redirect('login')
+        return redirect('register')
     else:
         return redirect('register')
